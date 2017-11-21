e6d2263df77a21b02d3db0fa44ea7e805385e4b6
madalitsomchaina <mmchain@uwo,ca>
2017-11-21 17:35:19 -0500

    All the arduino code and librarier

---
+++ libraries/BlueLight/BlueLight.cpp
@@ -0,0 +1,17 @@
+﻿#include<BlueLight.h>
+
+
+BlueLight::BlueLight(char pNum) : pinNumber(pNum) {
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, HIGH);
+}
+
+void BlueLight::on() {
+    digitalWrite(pinNumber, LOW);
+    pinState = true;
+}
+
+void BlueLight::off() {
+    digitalWrite(pinNumber, HIGH);
+    pinState = false;
+}
---
+++ libraries/BlueLight/BlueLight.h
@@ -0,0 +1,21 @@
+﻿#ifndef BlueLight_h
+#define BlueLight_h
+#if defined(ARDUINO) && ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#include <pins_arduino.h>
+#endif
+
+class BlueLight {
+
+    const char pinNumber;
+    bool pinState;
+
+public:
+    BlueLight(char pNum);
+    void on();
+    void off();
+
+};
+#endif
